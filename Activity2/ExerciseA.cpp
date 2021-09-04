4c658d4e477494a9dcc9239a2f9da11c7dd2ea42
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2021-09-05 02:47:38 +0900

    added exercise A in activity2

---
+++ Activity2/ExerciseA.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+const int SIZE_OF_ARRAY = 5;
+
+int* MyFunction(int *ptr){
+    cout << "address of 1st element: " << ptr << endl;
+    int *address_fifth_element = ptr + SIZE_OF_ARRAY - 1;
+    return address_fifth_element;
+}
+
+int main(){
+    int array[SIZE_OF_ARRAY];
+    int *q = MyFunction(array);
+    cout << "address of 5th element: " << q << endl;
+    return 0;
+}
