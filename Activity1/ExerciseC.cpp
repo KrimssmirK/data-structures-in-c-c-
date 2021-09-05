53d15912029275a7ef05fe4b96c3c7baced1e67f
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2021-09-06 00:19:29 +0900

    few modifications

--- Activity1/ExerciseC.cpp
+++ Activity1/ExerciseC.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 bool isVowel(char c){
     char vowels[] = {'a', 'e', 'i', 'o', 'u'};
     for(int i = 0; i < sizeof(vowels)/sizeof(char); i++){
@@ -28,7 +27,7 @@ bool isConsonant(char c){
 void getNoOfConsonantsAndVowelsAndSpaces(string input, int *vowels, int *consonants, int *spaces){
     for(int i = 0 ; i < input.length(); i++){
         char current_character = tolower(input[i]);
-        if(current_character == 'a' || current_character == 'e' || current_character == 'i' || current_character == 'o' || current_character == 'u'){
+        if(isVowel(current_character)){
             (*vowels)++;
         }else if(current_character == ' '){
             (*spaces)++;
@@ -38,41 +37,6 @@ void getNoOfConsonantsAndVowelsAndSpaces(string input, int *vowels, int *consona
     }
 }
 
-
-
-// int getNoOfConsonants(string input){
-//     int no_of_consonants = 0;
-//     for(int i = 0; i < input.length(); i++){
-//         char current_character = input[i];
-//         if(current_character != ' ' && current_character != 'a' && current_character != 'e' && current_character != 'i' && current_character != 'o' && current_character != 'u'){
-//             no_of_consonants++;
-//         }
-//     }
-//     return no_of_consonants;
-// }
-
-// int getNoOfVowels(string input){
-//     int no_of_vowels = 0;
-//     for(int i = 0; i < input.length(); i++){
-//         char current_character = input[i];
-//         if(current_character != ' ' && current_character == 'a' || current_character == 'e' || current_character == 'i' || current_character == 'o' || current_character == 'u'){
-//             no_of_vowels++;
-//         }
-//     }
-//     return no_of_vowels;
-// }
-
-// int getNoOfSpaces(string input){
-//     int no_of_spaces = 0;
-//     for(int i = 0; i < input.length(); i++){
-//         char current_character = input[i];
-//         if(current_character == ' '){
-//             no_of_spaces++;
-//         }
-//     }
-//     return no_of_spaces;
-// }
-
 int main(){
     string input;
     int no_of_consonants;
@@ -81,10 +45,6 @@ int main(){
 
     cout << "Enter characters: ";
     getline(cin, input);
-
-    // no_of_consonants = getNoOfConsonants(input);
-    // no_of_vowels = getNoOfVowels(input);
-    // no_of_spaces = getNoOfSpaces(input);
 
     getNoOfConsonantsAndVowelsAndSpaces(input, &no_of_vowels, &no_of_consonants, &no_of_spaces);
 
