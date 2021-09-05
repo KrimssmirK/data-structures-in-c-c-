63d271f123929754e6abca26247f0f1b7f0f9c55
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2021-09-06 04:03:43 +0900

    modified exerciseB in activity2

--- Activity2/ExerciseB.cpp
+++ Activity2/ExerciseB.cpp
@@ -3,52 +3,86 @@
 
 using namespace std;
 
-void showNoOfAppearance(string input){
+bool hasSeen(char c, char *ptr_c){
+    while(*ptr_c != '\0'){
+        if(*ptr_c == c){
+            return true;
+        }
+        ptr_c++;
+    }
+    return false;
+}
+
+void showNoOfAppearances(char *c, int size){
     cout << "Number of appearances" << endl;
-    for(int i = 0 ; i < input.length() ; i++){
-        char current_char = input[i];
-        if(current_char == '\0' || current_char == ' '){
+    char seen[size + 1];
+    for(int i = 0 ; i < size ; i++){
+        char current_char = c[i];
+        if(hasSeen(current_char, seen) || current_char == ' '){
             continue;
         }
         int count = 0;
-        for(int j = 0 ; j < input.length() ; j++){
-            if(input[j] == current_char){
+        for(int j = 0 ; j < size ; j++){
+            if(c[j] == current_char){
                 count++;
-                input[j] = '\0';
             }
         }
         cout << current_char << " -> " << count << endl;
+        seen[i] = current_char;
+    }
+    if(seen[0] != c[0]){
+        cout << "null";
     }
     cout << endl;
+    
 }
 
-void showIndices(string input){
+void showIndices(char *c, int size){
     cout << "Indices of the character" << endl;
-    for(int i = 0 ; i < input.length() ; i++){
-        char current_char = input[i];
-        if(current_char == '\0' || current_char == ' '){
+    char seen[size + 1];
+    for(int i = 0 ; i < size ; i++){
+        char current_char = c[i];
+        if(hasSeen(current_char, seen) || current_char == ' '){
             continue;
         }
         cout << current_char << " -> ";
-        for(int j = 0 ; j < input.length() ; j++){
-            if(input[j] == current_char){
+        for(int j = 0 ; j < size ; j++){
+            if(c[j] == current_char){
                 cout << j << " ";
-                input[j] = '\0';
             }
         }
         cout << endl;
+        seen[i] = current_char;
+    }
+    if(seen[0] != c[0]){
+        cout << "null";
     }
     cout << endl;
 }
 
 
 
-void changeStringToCharArray(char *c, string input){
-    for(int i = 0 ; i < input.length(); i++){
-        *c = input[i];
-        c++;
+void showAddress(char *c, int size){
+    cout << "Addresses of the character" << endl;
+    char seen[size + 1];
+    for(int i = 0; i < size; i++){
+        char current_char = c[i];
+        if(hasSeen(current_char, seen) || current_char == ' '){
+            continue;
+        }
+        cout << current_char << " -> ";
+        for(int j = 0; j < size; j++){
+            if(c[j] == current_char){
+                cout << (void*)&c[j] << " ";
+            }
+        }
+        cout << endl;
+        seen[i] = current_char;
     }
-    *c = '\0';
+    if(seen[0] != c[0]){
+        cout << "null";
+    }
+    cout << endl;
 }
 
 int main(){
@@ -56,10 +90,11 @@ int main(){
     cout << "Enter characters: ";
     getline(cin, input);
 
-    showNoOfAppearance(input);
+    showNoOfAppearances(&input[0], input.length());
     
-    showIndices(input);
-    
+    showIndices(&input[0], input.length());
+
+    showAddress(&input[0], input.length());
     
     return 0;
 }
