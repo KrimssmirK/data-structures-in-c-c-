154c4178183f7278afeea1bed7232be2766c1a9a
Romeo P. London III <romeothethird456@gmail.com>
2021-09-25 16:43:59 +0800

    analysis 2

--- Activity2/ExerciseA.cpp
+++ Activity2/ExerciseA.cpp
@@ -5,16 +5,16 @@ using namespace std;
 const int SIZE_OF_ARRAY = 10;
 const int UP_TO_FIFTH = 5;
 
-int* MyFunction(int *ptr){
+int *MyFunction(int *ptr){
     for(int i = 0; i < UP_TO_FIFTH; i++){
-        cout << "address of " << "element " << i + 1 << " " << ptr + i << endl;
+        cout << "address of element " << i + 1 << " " << ptr + i << endl;
     }
     int *address_fifth_element = ptr + 4;
     return address_fifth_element;
 }
 
 int main(){
-    int array[SIZE_OF_ARRAY];
+    int array[10];
     int *q = MyFunction(array);
     cout << "address of 5th element: " << q << endl;
     return 0;
--- c++ tutorial/test.cpp
+++ c++ tutorial/test.cpp
@@ -3,22 +3,33 @@
 
 using namespace std;
 
-struct Student{
-    string name;
-    int age;
-};
+// struct Student{
+//     string name;
+//     int age;
+// };
 
 int main(){
+
+    int x = 4;
+    int *ptr;
+    ptr = &x;
+    cout << x << endl;
+    cout << &x << endl;
+    
+   
+   
+    
+
     //struct
-    Student student1;
-    student1.name = "kenji";
-    student1.age = 22;
+    // Student student1;
+    // student1.name = "kenji";
+    // student1.age = 22;
 
-    cout << student1.name << endl;
+    // cout << student1.name << endl;
 
-    //array of structure
-    Student students[5];
-    students[0].name = "romeo";
+    // //array of structure
+    // Student students[5];
+    // students[0].name = "romeo";
 
 
 }
